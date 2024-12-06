b69b1464644c3ea0646c7829aa183e23ddd5554a
jannatun-nur <jannatunur03@gmail.com>
2024-12-06 18:01:09 +0600

    arrival 65%

--- src/Component/Arrival/ArrivalCard.jsx
+++ src/Component/Arrival/ArrivalCard.jsx
@@ -2,9 +2,10 @@ const ArrivalCard = ({ arrivals }) => {
     const { name, price, button } = arrivals;
 
     return (
-        <div style={{ border: "1px solid #ccc", padding: "10px", textAlign: "center" }}>
-            <h3>{name}</h3>
-            <p>Price: ${price}</p>
+        <div style={{ border: "1px solid #ccc", padding: "10px", textAlign: "center"
+        , background: "white" }}>
+            <h3 className="text-gray-800 font-semibold lg:text-xl py-2">{name}</h3>
+            <p className="text-gray-800 font-semibold lg:text-xl py-2">${price}</p>
             <button>{button}</button>
         </div>
     );
