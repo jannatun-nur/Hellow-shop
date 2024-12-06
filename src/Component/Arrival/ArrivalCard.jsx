bc7a86e9df150075317c8f85c89d9409e718548d
jannatun-nur <jannatunur03@gmail.com>
2024-12-06 18:15:21 +0600

    arrival 75%

--- src/Component/Arrival/ArrivalCard.jsx
+++ src/Component/Arrival/ArrivalCard.jsx
@@ -4,7 +4,7 @@ const ArrivalCard = ({ arrivals }) => {
     return (
         <div style={{ border: "1px solid #3939a3", padding: "10px", textAlign: "center"
         , background: "white" }}>
-            <h3 className="text-gray-800 font-semibold lg:text-xl py-2">{name}</h3>
+            <h3 className="text-gray-800 font-semibold text-xl py-2">{name}</h3>
 
             
               {[...Array(5)].map((_, index) => (
@@ -18,9 +18,9 @@ const ArrivalCard = ({ arrivals }) => {
                 </span>
               ))}
             
-            <p className="text-gray-800 font-semibold lg:text-xl py-4">${price}</p>
+            <p className="text-gray-800 font-semibold text-xl py-4">${price}</p>
             <button className="bg-blue-950 text-white
-            lg:px-5 lg:py-3 font-bold rounded-ee-xl">{button}</button>
+            px-5 py-3 font-bold rounded-ee-xl">{button}</button>
         </div>
     );
 };
