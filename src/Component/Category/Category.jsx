3fd38cb3feddc839ef4ebe1e839e1ccb13ee217f
jannatun-nur <jannatunur03@gmail.com>
2024-12-06 18:50:57 +0600

    category 40%

--- src/Component/Category/Category.jsx
+++ src/Component/Category/Category.jsx
@@ -12,13 +12,18 @@ const Category = () => {
       ];
 
     return (
-        <div className="image-grid">
+        <div className="mt-20">
+
+<p className='lg:text-2xl ml-3 font-bold text-blue-950 py-5'>CATEGORIES</p>
+
+            <div className="image-grid">
       {images.map((image, index) => (
         <a href={image.link} key={index} className="image-grid-item">
           <img src={image.src} alt={`Image ${index + 1}`} />
         </a>
       ))}
     </div>
+        </div>
     );
 };
 
