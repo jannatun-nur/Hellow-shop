016e46e624c80ff85e85635d3e3ee4b4b36c5c33
jannatun-nur <jannatunur03@gmail.com>
2024-12-06 16:39:30 +0600

    arrival 55%

--- src/Component/Arrival/ArrivalCard.jsx
+++ src/Component/Arrival/ArrivalCard.jsx
@@ -1,10 +1,37 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-const ArrivalCard = ({arrivals}) => {
+const ArrivalCard = ({ arrivals, deviceType }) => {
+    const responsive = {
+        desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
+        tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+        mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
+    };
 
-    const {name} = arrivals;
     return (
         <div>
-            
+            <Carousel
+                swipeable={false}
+                draggable={false}
+                showDots={true}
+                responsive={responsive}
+                ssr={true}
+                infinite={true}
+                autoPlay={deviceType !== "mobile"}
+                autoPlaySpeed={1000}
+                keyBoardControl={true}
+                customTransition="all .5"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                deviceType={deviceType}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+            >
+                {arrivals.map((item, index) => (
+                    <div key={index}>{item.name}</div>
+                ))}
+            </Carousel>
         </div>
     );
 };
