7ab79122470f45bce13d292b24343c9c1f9768ee
jannatun-nur <jannatunur03@gmail.com>
2024-12-07 08:41:50 +0600

    signup 40%

--- src/Component/SignUp/Signup.jsx
+++ src/Component/SignUp/Signup.jsx
@@ -1,51 +1,45 @@
-import bag from '../../../public/assets/images/bag.png'
-import signup from '../../../public/assets/images/signup.png'
-import './signup.css'
+import React from 'react';
+import bag from '../../../public/assets/images/bag.png';
+import signup from '../../../public/assets/images/signup.png';
+import './signup.css';
 
 const Signup = () => {
-    return (
-        <div>
-             <div className="container">
-      {/* Sidebar with form */}
+  return (
+    <div className="signup-container">
+      {/* Left-side form */}
       <div className="form-container">
-        <img
-          src={bag}
-          alt="logo"
-          className="form-logo"
-        />
-        <form className="form">
-          <input type="text" placeholder="NAME" className="form-input" />
-          <input type="email" placeholder="EMAIL" className="form-input" />
-          <input type="tel" placeholder="NUMBER" className="form-input" />
-          <input type="text" placeholder="ADDRESS" className="form-input" />
-          <input type="url" placeholder="IMAGE URL" className="form-input" />
-          <button type="submit" className="submit-button">SUBMIT</button>
-        </form>
-      </div>
+  <div className="form-content">
+    <img src={bag} alt="Bag Icon" className="form-logo" />
+    <form className="form">
+      <input type="text" placeholder="NAME" className="form-input" />
+      <input type="email" placeholder="EMAIL" className="form-input" />
+      <input type="tel" placeholder="NUMBER" className="form-input" />
+      <input type="text" placeholder="ADDRESS" className="form-input" />
+      <input type="url" placeholder="IMAGE URL" className="form-input" />
+      <button type="submit" className="submit-button">SUBMIT</button>
+    </form>
+  </div>
+</div>
+
 
       {/* Right-side image and icons */}
       <div className="image-container">
-        <img
-          src={signup}
-          alt="illustration"
-          className="illustration"
-        />
+        <img src={signup} alt="Signup Illustration" className="illustration" />
         <div className="social-icons">
           <img
-            src="https://via.placeholder.com/30"
+            src="https://img.icons8.com/color/48/google-logo.png"
             alt="Google"
             className="icon"
           />
           <img
-            src="https://via.placeholder.com/30"
+            src="https://img.icons8.com/ios-filled/48/000000/new-post.png"
             alt="Email"
             className="icon"
           />
         </div>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default Signup;
