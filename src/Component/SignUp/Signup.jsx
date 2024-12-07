51effc2ba4ee29c537d2473b2086cf64bfc345eb
jannatun-nur <jannatunur03@gmail.com>
2024-12-07 17:54:15 +0600

    signup 40%

--- src/Component/SignUp/Signup.jsx
+++ src/Component/SignUp/Signup.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import bag from '../../../public/assets/images/bag.png';
-import signup from '../../../public/assets/images/signup.png';
-import './signup.css';
+import React from "react";
+import bag from "../../../public/assets/images/bag.png";
+import signup from "../../../public/assets/images/signup.png";
+import "./signup.css";
 
 const Signup = () => {
   return (
     <div className="signup-container">
       {/* Left-side form */}
       <div className="form-container">
-  <div className="form-content">
-    <img src={bag} alt="Bag Icon" className="form-logo" />
-    <form className="form">
-      <input type="text" placeholder="NAME" className="form-input" />
-      <input type="email" placeholder="EMAIL" className="form-input" />
-      <input type="tel" placeholder="NUMBER" className="form-input" />
-      <input type="text" placeholder="ADDRESS" className="form-input" />
-      <input type="url" placeholder="IMAGE URL" className="form-input" />
-      <button type="submit" className="submit-button">SUBMIT</button>
-    </form>
-  </div>
-</div>
-
+        <div className="form-content">
+          <img src={bag} alt="Bag Icon" className="form-logo" />
+          <form className="form">
+            <input type="text" placeholder="NAME" className="form-input" />
+            <input type="email" placeholder="EMAIL" className="form-input" />
+            <input type="tel" placeholder="NUMBER" className="form-input" />
+            <input type="text" placeholder="ADDRESS" className="form-input" />
+            <input type="url" placeholder="IMAGE URL" className="form-input" />
+            <button type="submit" className="submit-button">
+              SUBMIT
+            </button>
+          </form>
+        </div>
+      </div>
 
       {/* Right-side image and icons */}
       <div className="image-container">
