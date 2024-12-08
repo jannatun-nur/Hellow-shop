9d14b721eb77ad57cee5019f8341e3a4d131add1
jannatun-nur <jannatunur03@gmail.com>
2024-12-08 16:37:38 +0600

    watch 30%

--- src/Component/Watch/Watch.jsx
+++ src/Component/Watch/Watch.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import WatchCard from "./WatchCard";
 
 
 const Watch = () => {
+    const [watch ,setWatch] = useState([])
+
+    useEffect(()=>{
+        fetch('shirt.json')
+        .then(res => res.json())
+        .then(data => setWatch(data))
+    },[])
     return (
         <div>
             
+
+            <div className="grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 gap-4 mx-4">
+                {
+                   watch.map( watchs=> <WatchCard key={watchs} watchs={watchs}></WatchCard>)
+                }
+            </div>
         </div>
     );
 };
--- src/Component/Watch/WatchCard.jsx
+++ src/Component/Watch/WatchCard.jsx
@@ -1,8 +1,33 @@
 
-const WatchCard = () => {
+const WatchCard = ({watchs}) => {
+    const {name , price, button} = shoes;
     return (
         <div>
+            <div style={{ boxShadow:"0px 4px 6px rgba(0, 0, 0, 0.7)", padding: "10px", textAlign: "center"
+        , background: "white" }}>
+            <h3 className="text-gray-800 font-semibold text-xl py-2">{name}</h3>
+
             
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  className={`text-3xl ${
+                    index < 4 ? "text-yellow-500" : "text-gray-300"
+                  }`}
+                >
+                  ★
+                </span>
+              ))}
+            
+            <p className="text-gray-800 font-semibold text-xl py-4">${price}</p>
+
+            <div>
+                <button className="text-blue-700 text-4xl mr-5"><MdOutlineShoppingCart /></button>
+                <button className="text-red-600 text-4xl mb-5"><FaHeartCirclePlus /></button>
+            </div>
+            <button className="bg-blue-950 text-white
+            px-5 py-3 font-bold rounded-ee-xl hover:bg-blue-900">{button}</button>
+        </div>
         </div>
     );
 };
