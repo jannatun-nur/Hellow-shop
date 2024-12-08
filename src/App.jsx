dc4a5e8eacf6c37ad22e75f5a987df75c98ca3c1
jannatun-nur <jannatunur03@gmail.com>
2024-12-08 09:23:52 +0600

    device 20%

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Banner from './Component/Banner/Banner'
 import Arrival from './Component/Arrival/Arrival'
 import Shirt from './Component/Shirt/Shirt'
-import Ladis from './Component/Ladis/Ladis'
+
 
 function App() {
   
---
+++ src/Component/Device/Device.jsx
@@ -0,0 +1,11 @@
+
+
+const Device = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Device;
