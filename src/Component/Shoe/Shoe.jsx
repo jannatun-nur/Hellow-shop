c1a008b295ded702a71c13d1d8cfa80a40fb4f4e
jannatun-nur <jannatunur03@gmail.com>
2024-12-08 16:27:17 +0600

    shoe 30%

--- src/Component/Shoe/Shoe.jsx
+++ src/Component/Shoe/Shoe.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import ShoeCard from "./ShoeCard";
 
 
 const Shoe = () => {
+    const [shoe , setShoe] = useState([])
+
+    useEffect(()=>{
+        fetch('shirt.json')
+        .then(res => res.json())
+        .then(data => setShoe(data))
+    },[])
     return (
         <div>
             
+
+            <div className="grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 gap-4 mx-4">
+                {
+                   shoe.map( shoes=> <ShoeCard key={shoes} shoes={shoes}></ShoeCard>)
+                }
+            </div>
         </div>
     );
 };
