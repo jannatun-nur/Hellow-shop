3fe44693eee388ad2a74fce04ce60741ada7fefb
jannatun-nur <jannatunur03@gmail.com>
2024-12-08 09:27:06 +0600

    device 30%

--- src/Component/Device/Device.jsx
+++ src/Component/Device/Device.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import DeviceCard from "./DeviceCard";
 
 
 const Device = () => {
+    const [device, setDevice] = useState([])
+
+    useEffect(()=>{
+        fetch('shirt.json')
+        .then(res => res.json())
+        .then(data => setDevice(data))
+    },[])
     return (
         <div>
             
+
+            <div className="grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 gap-4 mx-4">
+                {
+                    device.map( devices=> <DeviceCard key={devices} devices={devices}></DeviceCard>)
+                }
+            </div>
         </div>
     );
 };
