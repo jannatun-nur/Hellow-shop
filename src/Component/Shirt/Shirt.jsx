e288d3b384756cdabd6bcf27f0a629b5afe12f7f
jannatun-nur <jannatunur03@gmail.com>
2024-12-08 09:03:25 +0600

    shirt 30%

--- src/Component/Shirt/Shirt.jsx
+++ src/Component/Shirt/Shirt.jsx
@@ -15,7 +15,7 @@ const Shirt = () => {
         <div>
             
 
-            <div>
+            <div className="grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 gap-4 mx-4">
                 {
                     shirt.map( shirts=> <ShirtCard key={shirts} shirts={shirts}></ShirtCard>)
                 }
