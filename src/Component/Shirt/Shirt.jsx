eed53bf50938af54d26fe6aa0b6c4f68f0b0c599
jannatun-nur <jannatunur03@gmail.com>
2024-12-08 07:48:31 +0600

    shirt 25%

--- src/Component/Shirt/Shirt.jsx
+++ src/Component/Shirt/Shirt.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import ShirtCard from "./ShirtCard";
 
 
 const Shirt = () => {
+
+    const [shirt, setShirt] = useState([])
+
+    useEffect(()=>{
+        fetch('shirt.json')
+        .then(res => res.json())
+        .then(data => setShirt(data))
+    },[])
     return (
         <div>
             
+
+            <div>
+                {
+                    shirt.map( shirts=> <ShirtCard key={shirts} shirts={shirts}></ShirtCard>)
+                }
+            </div>
         </div>
     );
 };
